3073ccb9c07681c9939410795a1340bb668b3666
faiq nasir <faiqnasir@faiqs-MBP.lan1>
2021-03-02 14:03:35 -0500

    readme file description

--- Article_Scrapper.py
+++ Article_Scrapper.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
 
 
 import requests
@@ -11,26 +10,15 @@ import pandas as pd
 result = requests.get('https://www.foxnews.com/politics/biden-harris-victory-speech-46th-president-united-states')
 
 
-# In[2]:
-
-
 soup = BeautifulSoup(result.content, 'html.parser')
 
-
-# In[3]:
 
 
 body = soup.findAll('body' , {'class':re.compile('fn article-single')})
 body1= body[0].text.strip()
 
 
-# In[4]:
-
-
 print(body1)
-
-
-# In[5]:
 
 
 headline = soup.select('h1')
@@ -39,27 +27,13 @@ print(headline1)
 print('')
 
 
-# In[7]:
-
-
 # Create a dataframe
 data = pd.DataFrame({'Headline': headline1, 'Body': body1}, index=[1])
 data
 
 
-# In[8]:
-
-
 data.to_csv('FOXNEWS_DF.csv')
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # BYGB7990
-Test description
+
+
+The article_scrapper aims at scraping the information from different news article sites. 
+
+Scraping:
+
+The purpose of the this python function is to separate the headlines and the body of each article. 
+Once you run the program on an article  for instance, from Fox News. 
+The program will use the url for the article we want to scrape and bring back just the 
+headline and the body of the article separated. 
+
+Dataframe:
+
+Once the information is scraped then the program goes and compiles all the information
+in a dataframe. And then this dataframe is saved in a CSV file that is stored on the local
+machine. 
+
